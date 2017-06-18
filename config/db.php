29bf35634d125b4f10beeabea32629e5dc2c1487
Alexandre <alexandrefilho2005@gmail.com>
2017-06-17 23:42:06 -0300

    First commit for verification

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'development' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database' => 'son_financas',
+			'username' => 'root',
+			'password' => 'Ale!135(',
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci'
+	]
+];
